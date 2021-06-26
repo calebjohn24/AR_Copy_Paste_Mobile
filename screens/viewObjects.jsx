e7c583ffdfecd5bb12c7da5fb28c0253962b2271
Caleb John <cajohn0205@gmail.com>
2021-06-26 12:51:20 -0700

    finshed mobile app, with deletion and download

--- screens/viewObjects.jsx
+++ screens/viewObjects.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import { useState, useEffect, useRef } from "react";
-import { Text, View, Image, ActivityIndicator, FlatList } from "react-native";
+import {
+  Text,
+  View,
+  Image,
+  ActivityIndicator,
+  FlatList,
+  Share,
+  Linking,
+} from "react-native";
 import styles from "../styles/viewObjects";
 import { TouchableOpacity } from "react-native-gesture-handler";
 
@@ -45,30 +53,171 @@ export default function ViewObjects({ navigation }) {
 
   const _renderItem = (item, index) => {
     return (
-      <View key={item['index']}>
-          <Image
-            source={{
-              // uri:
-              //   "https://source.unsplash.com/random/500x" +
-              //   Math.floor(Math.random() * 800 + 500)
+      <View key={item["index"]}>
+        <Image
+          source={{
+            // uri:
+            //   "https://source.unsplash.com/random/500x" +
+            //   Math.floor(Math.random() * 800 + 500)
 
-              uri: item['item']
+            uri: item["item"],
+          }}
+          style={{ resizeMode: "contain", height: 250 }}
+        />
+        <View
+          style={{
+            height: 100,
+            flex: 1,
+
+            flexDirection: "row",
+            width: "100%",
+            height: 100,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <TouchableOpacity onPress={() => deleteImg(item["item"], item["index"])}>
+            <Image
+              source={require("../assets/images/trash-orange.png")}
+              fadeDuration={0}
+              style={{ height: "70%", resizeMode: "contain" }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => {
+              Linking.openURL(item["item"]);
             }}
-            style={{resizeMode: "contain", height:200 }}
-          />
+          >
+            <Image
+              source={require("../assets/images/dowload-purple.png")}
+              fadeDuration={0}
+              style={{ height: "70%", resizeMode: "contain" }}
+            />
+          </TouchableOpacity>
+        </View>
       </View>
-    )
-  }
+    );
+  };
 
   const _renderItemText = (item, index) => {
-      console.log(item);
     return (
-      <View key={item['index']} style={{borderColor:'#F59E0B',borderBottomWidth:1,borderTopWidth:1, paddingHorizontal:20, paddingVertical:8}}>
-          <Text>{item['item']["data"]}</Text>
-      </View>
-    )
-  }
+      <View
+        key={item["index"]}
+        style={{
+          borderColor: "#F59E0B",
+          borderBottomWidth: 1,
+          borderTopWidth: 1,
+          paddingHorizontal: 20,
+          paddingVertical: 8,
+        }}
+      >
+        <Text>{item["item"]["data"]}</Text>
+        <View
+          style={{
+            height: 100,
+            flex: 1,
 
+            flexDirection: "row",
+            width: "100%",
+            height: 100,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <TouchableOpacity onPress={() => deleteText(item["item"]["id"], item["index"])}>
+            <Image
+              source={require("../assets/images/trash-orange.png")}
+              fadeDuration={0}
+              style={{ height: "70%", resizeMode: "contain" }}
+            />
+          </TouchableOpacity>
+          <TouchableOpacity onPress={() => onShareText(item["item"]["data"])}>
+            <Image
+              source={require("../assets/images/share-purple.png")}
+              fadeDuration={0}
+              style={{ height: "70%", resizeMode: "contain" }}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  };
+
+  const onShareText = async (msg) => {
+    try {
+      const result = await Share.share({
+        message: msg,
+      });
+      if (result.action === Share.sharedAction) {
+        if (result.activityType) {
+          // shared with activity type of result.activityType
+        } else {
+          // shared
+        }
+      } else if (result.action === Share.dismissedAction) {
+        // dismissed
+      }
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const deleteText = (id, index) => {
+      setSpinner(true);
+    var textListTmp = textList
+    textListTmp.splice(parseInt(index), 1);
+    setTextList(textListTmp);
+    fetch("https://e6721e636491.ngrok.io/api/delete_text", {
+      method: "Delete",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        delId: id,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          console.log(response.status);
+        }
+        setSpinner(false);
+        return response.json();
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {});
+  };
+
+  const deleteImg = async (link, index) => {
+      setSpinner(true);
+    console.log(link);
+    var imgListTmp = imageList
+    imgListTmp.splice(parseInt(index), 1);
+    setImageList(imgListTmp);
+    fetch("https://e6721e636491.ngrok.io/api/delete_img", {
+      method: "Delete",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        delUrl: link,
+      }),
+    })
+      .then((response) => {
+        if (!response.ok) {
+          console.log(response.status);
+        }
+        setSpinner(false);
+        return response.json();
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {});
+  };
 
   return (
     <View style={styles.container}>
@@ -98,7 +247,7 @@ export default function ViewObjects({ navigation }) {
                 fadeDuration={0}
                 style={styles.trayIcon}
               />
-            </TouchableOpacity >
+            </TouchableOpacity>
 
             <TouchableOpacity onPress={() => setShowImgs(true)}>
               <Image
@@ -109,35 +258,35 @@ export default function ViewObjects({ navigation }) {
             </TouchableOpacity>
           </View>
 
-          {showImgs?
-
-          <FlatList
-          data={imageList}
-          keyExtractor={(item, index) => "" + index}
-          style={{position: "absolute",
-          left: 0,
-          right: 0,
-          bottom: 108,
-          width: "100%",
-          height: "70%",}}
-          renderItem={_renderItem}
-        />:
-
-        <FlatList
-        data={textList}
-        keyExtractor={(item, index) => "" + index}
-        style={{position: "absolute",
-        left: 0,
-        right: 0,
-        bottom: 108,
-        width: "100%",
-        height: "70%",}}
-        renderItem={_renderItemText}
-      />}
-
-
-
-
+          {showImgs ? (
+            <FlatList
+              data={imageList}
+              keyExtractor={(item, index) => "" + index}
+              style={{
+                position: "absolute",
+                left: 0,
+                right: 0,
+                bottom: 108,
+                width: "100%",
+                height: "70%",
+              }}
+              renderItem={_renderItem}
+            />
+          ) : (
+            <FlatList
+              data={textList}
+              keyExtractor={(item, index) => "" + index}
+              style={{
+                position: "absolute",
+                left: 0,
+                right: 0,
+                bottom: 108,
+                width: "100%",
+                height: "70%",
+              }}
+              renderItem={_renderItemText}
+            />
+          )}
         </>
       )}
 
